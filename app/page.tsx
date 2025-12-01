9c1a6492ed9248e550b6f7d0144e5433109a2bba
Dávid Huljak <david.huljak@gmail.com>
2025-12-01 01:30:08 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,54 @@
-import Image from "next/image";
+import QRCode from "react-qr-code";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const date = new Date();
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  const qrCodeValue = year + month + day;
+  const textCodeValue = day + month;
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className={styles.mainContainer}>
+      <div className={styles.contentWrapper}>
+        <div className={styles.headerBox}>
+          <h1 className={styles.headerText}>TOALETY BB</h1>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+
+        <p className={styles.introText}>
+          ODEMKNĚTE SI
+          <br />
+          TOALETY!
+        </p>
+
+        <div className={styles.qrSection}>
+          <div className={styles.qrWrapper}>
+            <QRCode
+              value={qrCodeValue}
+              size={180}
+              style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+              viewBox={`0 0 256 256`}
+              fgColor="#222222"
+              bgColor="#ffffff"
             />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+          <p className={styles.qrText}>Dnešní kód - {textCodeValue}</p>
         </div>
-      </main>
-    </div>
+      </div>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://david.huljak.cz"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.footerLink}
+        >
+          Made by Dávid Huljak
+        </a>
+      </footer>
+    </main>
   );
 }
